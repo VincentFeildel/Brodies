fbc5616f2c22b20ae5a361d6903bc6ba69b3f162
Vincent Feildel <vincent.feildel@gmail.com>
2017-08-16 14:02:06 +0200

    création de la page booking et correction seed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pricing
+  validates :nb_persons, inclusion: { in: (0..10).to_a }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :activities do
-    resources :pricings
+    resources :bookings
   end
 
   root to: 'activities#home'
