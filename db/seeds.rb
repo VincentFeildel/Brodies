0b87808f955ad58c7123cfae4e616a018a232bc9
Florent Lienard <f.lienard@tbs-education.org>
2017-08-18 16:38:17 +0200

    Final version

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,7 +101,7 @@ Pricing.create(description: "Demi-journée, pique-nique inclus", amount: 40, pri
 Pricing.create(description: "Deux jours complets avec bivouac le soir", amount: 70, price_type: "par personne", activity_id: a.id)
 
 # ACTIVITE--------------------------------------------------------------------
-supplier = User.create(email: "supplier@supplier.com", password: "123456", is_supplier: true)
+supplier = User.create(email: "supplier12@user.com", password: "123456", is_supplier: true)
 
 a = Activity.new(is_outdoor: true, capacity: 10)
 a.user = supplier
@@ -121,3 +121,80 @@ Maîtrise des techniques de navigation en canoë"
 a.save
 Pricing.create(description: "Demi-journée, pique-nique inclus", amount: 60, price_type: "par personne", activity_id: a.id)
 Pricing.create(description: "Deux jours complets avec bivouac le soir", amount: 70, price_type: "par personne", activity_id: a.id)
+
+# ACTIVITE KARTING--------------------------------------------------------------------
+supplier = User.create(email: "supplier13@user.com", password: "123456", is_supplier: true)
+
+a = Activity.new(is_outdoor: true, capacity: 10)
+a.user = supplier
+a.title = 'Aerokart'
+a.category = 'Kart'
+a.address = '58, boulevard Vauban'
+a.city = 'Lille'
+a.zip_code = '59000'
+a.photo_url = 'http://res.cloudinary.com/dfoe2be36/image/upload/v1503066472/karting-flo_zxjcm4.jpg'
+a.description = "Le centre de karting de Lille est en intérieur, ce qui vous permettra de vous amusez au volant d'un kart même en plein hiver !
+Grâce à une équipe aux petits soins, vous prendrez du plaisir en toute sécurité.
+Vous prouverez vos talents de pilote grâce aux karts 4 temps de grande qualité, sur une piste intérieure de 400 mètres parfaitement entretenue.
+Que vous soyez grand pilote ou débutant, cette piste de Karting saura satisfaire et vous procurer des sensations extrêmes !
+
+L'activité est également accessible aux enfants de 7 à 14 ans (1m20 minimum)."
+a.save
+Pricing.create(description: "Demi-journée, pique-nique inclus", amount: 45, price_type: "par personne", activity_id: a.id)
+Pricing.create(description: "Deux jours complets avec bivouac le soir", amount: 90, price_type: "par personne", activity_id: a.id)
+
+# ACTIVITE RANDO (IMAGE A CHANGER)--------------------------------------------------------------------
+supplier = User.create(email: "supplier14@user.com", password: "123456", is_supplier: true)
+
+a = Activity.new(is_outdoor: true, capacity: 10)
+a.user = supplier
+a.title = 'Les calanques du soleil'
+a.category = 'Randonnée'
+a.address = '38 route de la Sablière'
+a.city = 'Marseille'
+a.zip_code = '13000'
+a.photo_url = 'http://res.cloudinary.com/dfoe2be36/image/upload/v1503066865/patrick-schneider-227160_mprpfz.jpg'
+a.description = "De Marseille à Cassis, les calanques – du provençal calenco, escarpé – se découpent en Méditerranée, véritables failles de calcaire sur le bleu éblouissant de la mer. Un site qui demande à être protégé face aux 2 millions de visiteurs annuels, si bien que les calanques viennent d'être classées Parc National : respect de l'environnement obligatoire, sous peine d'amende. Allez-y de préférence hors saison.
+
+Ces 28 balades sont à pratiquer avec de bonnes chaussures de marche et une protection contre le soleil ou le vent. Ne vous contentez pas d’une simple tenue de bain. Les cailloux roulent et sont dangereux. Le soleil tape. Prévoyez de l’eau potable et ne laissez aucune ordure derrière vous.
+
+Itinéraires
+
+- Commencez par découvrir le belvédère de Saint-Michel (246 m) en famille (4,5 km) au-dessus de Luminy, près de Marseille. Il vous donnera une approche du paysage sur la calanque de Morgiou et ses cabanons.
+
+- Vous vous enfoncerez dans la calanque de Sugiton (7 km) et son rocher du Torpilleur. Depuis la colline de Lun (3,5 km), la calanque de Sormiou apparaît derrières les lentisques et les térébinthes.
+
+- Le tour du massif de Marseilleveyre (12 km) est réservé aux sportifs, récompensés par des panoramas à couper le souffle.
+
+- La balade de Callelongue (9 km) longe sa calanque et celles de la Mounine et de Marseilleveyre.
+
+- L’intérieur montagnard du mont Puget, du belvédère d’En-Vau et des falaises du Devenson attirent les varappeurs de haut niveau.
+
+- Plus près de Cassis, explorez les très belles calanques d’En-Vau, de Port-Pin et de Port-Miou, avant de prendre un pastis mérité à Cassis, à l’ombre du cap Canaille (394 m)."
+a.save
+Pricing.create(description: "Demi-journée, pique-nique inclus", amount: 45, price_type: "par personne", activity_id: a.id)
+Pricing.create(description: "Deux jours complets avec bivouac le soir", amount: 90, price_type: "par personne", activity_id: a.id)
+
+# ACTIVITE RANDO--------------------------------------------------------------------
+supplier = User.create(email: "supplier15@user.com", password: "123456", is_supplier: true)
+
+a = Activity.new(is_outdoor: true, capacity: 10)
+a.user = supplier
+a.title = 'Pyrénées occidentales'
+a.category = 'Randonnée'
+a.address = '32, Rue de Viele'
+a.city = 'Pau'
+a.zip_code = '64000'
+a.photo_url = 'http://res.cloudinary.com/dfoe2be36/image/upload/v1503066472/squirrel-nager-cute-nature-158452_s9sbdj.jpg'
+a.description = "Une courte traversée (67 km) par le GR® 8 de Urt à Sare, et les collines basques qui environnent la Rhune, met en condition. Déjà, les maisons blanches à colombages, aux volets verts ou rouges, sont omniprésentes.
+
+Le GR® 10 commence à Hendaye, au bord de l’océan à qui vous tournez bientôt le dos. Cols et ventas mènent par Aïnhoa, Bidarray, Saint-Étienne-de-Baïgorry et Saint-Jean-Pied-de-Port, en montant et descendant. Petits chevaux Pottoks, têtes noires des brebis Manech : vous traversez le pays de Ramuntcho en Labourd, puis la Basse-Navarre.
+
+Saluons le sentier de Compostelle qui s’apprête à franchir les Pyrénées et continuons vers la Soule et la sauvage forêt d’Iraty. Sous le passage des palombes, les crêtes se font plus hautes à Larau, les crevasses deviennent canyons à Kakouetta et Holçarté.
+
+Apprenez à différencier les cayolars ou cabanes de bergers basques, des cujalas béarnais, en pénétrant sur les terres d’Henri IV, en vallée de Barétous. Le karst d’Arette-la-Pierre-Saint-Martin, célèbre pour ses explorations spéléologiques, est unique en Europe.
+
+Un paysage lunaire qui conduit au pic d’Anie, porte du parc national des Pyrénées. Vallée d’Aspe et lacs d’Ayous, vallée d’Ossau et lacs de Soussouéou et d’Anglas, l’isard et le gypaète barbu vous observent… de loin et saluent votre entrée sur les premiers cols bigourdans, jusqu’à Arrens."
+a.save
+Pricing.create(description: "Demi-journée, pique-nique inclus", amount: 45, price_type: "par personne", activity_id: a.id)
+Pricing.create(description: "Deux jours complets avec bivouac le soir", amount: 90, price_type: "par personne", activity_id: a.id)
